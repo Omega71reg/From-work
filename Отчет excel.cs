f3b2bf7e6434037134cf6aa7fae0ad4298448cce
Omega71reg <Omega71reg@Mail.ru>
2023-04-06 21:51:12 +0300

    Push

---
+++ Отчет excel.cs
@@ -0,0 +1,78 @@
+﻿public static void ОтчетПоКатегориямПВК(БланкДляВводаПланаПараметры бланкДляВводаПланаПараметры)
+{
+    /*if (бланкДляВводаПланаПараметры.ПустойБланк)
+    {
+        new Dictionary<string, object>().PrintToExcelTemplate("ПустойБланкДляВводаПлана");
+        return;
+    }*/
+    List<ПозицияПланаВыпускаВКомплектахПоПроизводственнойВедомости> ПозПВК = new List<ПозицияПланаВыпускаВКомплектахПоПроизводственнойВедомости>();
+    using (UnitOfWork uow = new UnitOfWork())
+    {
+        CriteriaOperator КритерийОтбора = CriteriaOperator.And
+        (
+            CriteriaOperator.Parse("ПроизводственнаяВедомость.ГруппаЗаказов Is Null"),
+            CriteriaOperator.Or
+            (
+            new BinaryOperator("ПроизводственнаяВедомость", uow.GetObject(бланкДляВводаПланаПараметры.ПроизводственнаяВедомость)),
+            new BinaryOperator("ПроизводственнаяВедомость.Тема", uow.GetObject(бланкДляВводаПланаПараметры.Тема)),
+            new BinaryOperator("ПроизводственнаяВедомость.ОтветственныйОтПроизводственноДиспетчерскогоОтдела", uow.GetObject(бланкДляВводаПланаПараметры.ОтветственныйОтПдо))
+            )
+        );
+        var tmpList1 = uow.GetObjects<ПозицияПланаВыпускаВКомплектахПоПроизводственнойВедомости>(КритерийОтбора);
+
+        if (tmpList1 != null)
+        {
+            ПозПВК = tmpList1.ToList();
+        }
+    }
+    var themes = ПозПВК.GroupBy(x => x.ПроизводственнаяВедомость?.Тема()).OrderBy(x => x.First().ПроизводственнаяВедомость?.Тема()?.Наименование).ToList();
+    int themesCount = themes.Count;
+    if (themesCount == 0)
+    {
+        return;
+    }
+    ExcelDocument excelDocument = new ExcelDocument { TemplateName = "БланкДляВводаПланаТЕСТ" };
+    excelDocument.Init();
+    excelDocument.CloneSheetAfter(1, excelDocument.SheetsCount, themesCount - 1);
+    string[] monthes = DateTimeExtensions.GetMonthes();
+
+    for (int i = 1; i <= themesCount; i++)
+    {
+        var theme = themes[i - 1];
+        excelDocument.SetSheetName(i, String.IsNullOrEmpty(theme.First().ПроизводственнаяВедомость.Тема()?.Наименование) ? "Без темы" : theme.First().ПроизводственнаяВедомость.Тема()?.Наименование);
+
+        excelDocument.SetSheetRangeByMarker(i, "#ТемаНаименованиеSheetNumber#", $"#ТемаНаименование{i}#");
+        excelDocument.SetSheetRangeByMarker(i, "#ОтветственныйПдоНаименованиеSheetNumber#", $"#ОтветственныйПдоНаименование{i}#");
+        excelDocument.SetSheetRangeByMarker(i, "#ТаблицаSheetNumber#", $"#Таблица{i}#");
+
+        List<ExcelCellValueAndStyle[]> sheetData = new List<ExcelCellValueAndStyle[]>();
+        var ведомости = ПозПВК?.GroupBy(x => x.ПроизводственнаяВедомость).OrderBy(x => x.First().ПроизводственнаяВедомость.Номер).ToList();
+
+        foreach (var ведомость in ведомости.Where(x => x.Key.Тема() == theme.Key))
+        {
+            int curCount = sheetData.Count;
+            sheetData.AddRange(ведомость.OrderBy(x => x.ПроизводственнаяЕдиница?.Код).ThenBy(x => x.Наименование).Select(x => new[] {
+
+                        x.ПроизводственнаяВедомость?.Проект?.Номер.PackExcelData(),
+                        x.ПроизводственнаяВедомость.Номер.PackExcelData(),
+                        x.Наименование.PackExcelData(),
+                        x.ПроизводственнаяЕдиница.КраткоеНаименование().PackExcelData(),
+                        x.КоличествоКомплектовНаПроизводственнуюВедомость.PackExcelData(),
+                        x.КоличествоКомплектовФакт.PackExcelData(),
+                        "".PackExcelData(),
+                        "".PackExcelData(),
+                        "".PackExcelData(),
+                    }));
+            //sheetData[curCount][0].Value = ведомость.First().ПроизводственнаяВедомость.Номер;
+            ExcelCellValueAndStyle.ClearRepeatedCellValues(sheetData, curCount, true, true, false, 1, 2, 3);
+            sheetData.Add(new ExcelCellValueAndStyle[] { });
+        }
+
+        excelDocument.AddData($"ТемаНаименование{i}", $"Тема: {themes[i - 1].Key.Наименование}".PackExcelData());
+        excelDocument.AddData($"ОтветственныйПдоНаименование{i}", $"Ответственный сотрудник ПДО: {themes[i - 1].Where(x => x.ПроизводственнаяВедомость?.ОтветственныйОтПроизводственноДиспетчерскогоОтдела() != null).GroupBy(x => x.ПроизводственнаяВедомость?.ОтветственныйОтПроизводственноДиспетчерскогоОтдела()).ToString(y => y.Key.Наименование)}".PackExcelData());
+        excelDocument.AddData($"Таблица{i}", new ExcelTable { ReportRows = sheetData });
+    }
+    excelDocument.Print();
+    excelDocument.Show();
+}
+    }
---
+++ Печатная форма.cs
@@ -0,0 +1,266 @@
+﻿using Galaktika.Kbp.Amm.Module.Models.ПланВыпускаОснастки;
+using DevExpress.ExpressApp;
+using Galaktika.Kbp.Amm.Module.Models.Planning;
+using System.Collections.Generic;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using Galaktika.Kbp.Amm.Module.Utils;
+using System.Linq;
+using Galaktika.Kbp.Amm.Module.Models;
+using Galaktika.PRM.WOM.Module;
+using Galaktika.PRM.ATP.Module;
+using Galaktika.PRM.BOM.Module;
+using System.Data;
+using Galaktika.Kbp.Amm.Module.Models.Extensions;
+using System;
+using Galaktika.Core.Module;
+using Galaktika.PRM.Cost.Module;
+using Galaktika.Kbp.Amm.Module.Controllers.StateMachine;
+using Galaktika.Core.CD.Module.RES;
+
+
+static List<ЗаказПотребность> позицииЗПР = new List<ЗаказПотребность>();
+static ЗаказНаСдачуПродукции ШапкаЗПР = null;
+
+
+public string[] ToArray(БазовыйТехнологическийЭтап item)
+{
+    var result = new string[]
+    {
+                String.Format("{0}", item.ТехнологическоеОписание.ПредметПроизводства.Наименование),
+                String.Format("{0:n0}",
+                    (
+                    item.ТехнологическоеОписание.КоличествоНаТехОтработку() > 0 ?
+                    String.Format("{0:n0} + {1:n0}", item.ТехнологическоеОписание.КоличествоДсеНаПв(), item.ТехнологическоеОписание.КоличествоНаТехОтработку()) :
+                    String.Format("{0:n0}", item.ТехнологическоеОписание.КоличествоДсеНаПв())
+                    )),
+                String.Format("{0}", item.Место.КраткоеНаименование()),
+                String.Format("{0:n5}", item.ШтучноеВремя),
+            String.Format("{0}", item.Статус())
+    };
+    return result;
+}
+
+public string[] ToArray2(Tuple<ПроизводственнаяЕдиница, decimal> item)
+{
+
+    var result = new string[]
+    {
+                String.Format("{0}",item.Item1.Наименование) ,
+                String.Format("{0:n5}", item.Item2)
+    };
+    return result;
+}
+DataTable ConvertToDataTable1(List<БазовыйТехнологическийЭтап> list)
+{
+    DataTable table = new DataTable();
+
+    table.Columns.Add("Содержание и объем работ", typeof(string));
+    table.Columns.Add("Кол-во", typeof(string));
+    table.Columns.Add("Цех", typeof(string));
+    table.Columns.Add("Плановая трудоемкость", typeof(string));
+    table.Columns.Add("Согласование ОТН УНиОТ", typeof(string));
+    foreach (var item in list.OrderBy(row => row.ТехнологическоеОписание.ПредметПроизводства.Наименование))
+    {
+        table.Rows.Add(ToArray(item));
+    }
+    return table;
+}
+
+DataTable ConvertToDataTable2(List<БазовыйТехнологическийЭтап> list)
+{
+    DataTable table = new DataTable();
+
+    table.Columns.Add("Цех", typeof(string));
+    table.Columns.Add("Плановая трудоемкость", typeof(string));
+    //var a = list.GroupBy(x => x.Место);
+    /*var query = list.GroupBy(
+        x => x.Место, (a,data) => new 
+        {
+            группацех = a.Наименование,
+            суммавремя = data.Sum(x => x.ШтучноеВремя)
+        }
+        ).ToList();*/
+    foreach (var item in list.GroupBy(x => x.Место))
+    //.OrderBy(row => row.ТехнологическоеОписание.ПредметПроизводства.Наименование))
+    {
+        table.Rows.Add(ToArray2(new Tuple<ПроизводственнаяЕдиница, decimal>(item.Key, item.Sum(x => x.ШтучноеВремя))));
+    }
+    return table;
+}
+string ПВ1 = "", заказы = "";
+decimal a = 0;
+
+private void ЗаполнениеТаблицы_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+{
+    ШапкаЗПР = GetCurrentRow() as ЗаказНаСдачуПродукции;
+    //System.Windows.Forms.MessageBox.Show((ШапкаЗПР.Oid).ToString(), "Позиции");
+
+    if (ШапкаЗПР.ГруппаЗаказов == null)
+    {
+        System.Windows.Forms.MessageBox.Show(String.Format("Это не ЗПР"), "Oops...", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation);
+        this.StopPageBuilding();
+        return;
+    }
+
+    using (UnitOfWork uow = new UnitOfWork())
+    {
+        CriteriaOperator критерия = CriteriaOperator.And
+            (
+            new BinaryOperator("ЗаказНаСдачуП.Oid", ШапкаЗПР.Oid)
+            );
+
+        var tmpList1 = uow.GetObjects<ЗаказПотребность>(критерия);
+
+        if (tmpList1 != null)
+        {
+            позицииЗПР = tmpList1.Where(x => x.ТехОписание != null).ToList();
+            //System.Windows.Forms.MessageBox.Show((позицииЗПР.Count).ToString(),"Позиции")
+        }
+
+        ПВ1 = string.Join(", ", позицииЗПР.Where(item => item.ЗаказГП != null).GroupBy(item => item.ЗаказГП).Select(item => item.FirstOrDefault().ЗаказГП.Номер));
+        ПВ.Text = ПВ1.ToString();
+
+        заказы = позицииЗПР.Count > 0 ?
+              string.Join(", ", позицииЗПР.GroupBy(item => item.Проект).Select(item => item.FirstOrDefault().Проект.Номер)) :
+              ШапкаЗПР.Проект.Номер;
+        Заказ.Text = заказы;
+
+
+        //System.Windows.Forms.MessageBox.Show(str);
+        List<БазовыйТехнологическийЭтап> цехозаходы2 = позицииЗПР.SelectMany(x => x.ТехОписание.МаршрутБезИсключенныхВидовРабот()).ToList();
+
+        a = цехозаходы2.Sum(x => x.ШтучноеВремя);
+        label28.Text = String.Format("{0:n5}", a);
+
+        DataTable table1 = ConvertToDataTable1(цехозаходы2);
+        DetailReport.DataSource = table1;
+
+        DataTable table2 = ConvertToDataTable2(цехозаходы2);
+        DetailReport3.DataSource = table2;
+    }
+    tableCell5.DataBindings.Add("Text", null, "Содержание и объем работ");
+    tableCell11.DataBindings.Add("Text", null, "Кол-во");
+    tableCell6.DataBindings.Add("Text", null, "Цех");
+    tableCell7.DataBindings.Add("Text", null, "Плановая трудоемкость");
+    tableCell8.DataBindings.Add("Text", null, "Согласование ОТН УНиОТ");
+
+    tableCell10.DataBindings.Add("Text", null, "Цех");
+    tableCell12.DataBindings.Add("Text", null, "Плановая трудоемкость");
+}
+
+private void ЗаполнениеШапки_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+{
+    ШапкаЗПР = GetCurrentRow() as ЗаказНаСдачуПродукции;
+
+    if (ШапкаЗПР.ГруппаЗаказов == null)
+    {
+        System.Windows.Forms.MessageBox.Show(String.Format("Это не ЗПР"), "Oops...", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation);
+        this.StopPageBuilding();
+        return;
+    }
+
+
+    if (ШапкаЗПР != null)
+    {
+        подразделениеАвтора.Text = ШапкаЗПР.CreateEmployeeCalc().Сектор().Наименование;
+
+        ДатаИсполнения.Text = String.Format("от {0} № {1}",
+            ШапкаЗПР.ДатаДокумента,
+            ШапкаЗПР.Номер);
+
+        /*ВходящийНомер.Text = String.Format("Входящий номер в производстве {0}",
+            ШапкаЗПР.GetMemberValue("ВходящийНомерВПроизводстве"));*/
+    }
+}
+
+
+private void ЗаполнениеПодвал_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+{
+    ШапкаЗПР = GetCurrentRow() as ЗаказНаСдачуПродукции;
+
+    if (ШапкаЗПР.ГруппаЗаказов == null)
+    {
+        System.Windows.Forms.MessageBox.Show(String.Format("Это не ЗПР"), "Oops...", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation);
+        this.StopPageBuilding();
+        return;
+    }
+
+
+    if (ШапкаЗПР != null)
+    {
+        label31.Text = ШапкаЗПР.CreateEmployeeCalc().Сектор().Руководитель == null ? "" : ШапкаЗПР.CreateEmployeeCalc().Сектор().Руководитель.GetMemberValue("Наименование").ToString();
+
+        /*label31.Text = String.Format("Начальник {0}",
+            ШапкаЗПР.CreateEmployeeCalc().Сектор().Наименование);*/
+
+        label30.Text = String.Format("Исп. {0}",
+                ШапкаЗПР.CreateEmployeeCalc().Наименование);
+
+        /*ВходящийНомер.Text = String.Format("Входящий номер в производстве {0}",
+            ШапкаЗПР.GetMemberValue("ВходящийНомерВПроизводстве"));*/
+
+        string name2 = ШапкаЗПР.CreateEmployeeCalc().Сектор().Наименование as string;
+        string result = "Начальник " + name2.Replace("ОТДЕЛЕНИЕ", "отделения");
+        label29.Text = result;
+
+        if (ШапкаЗПР.СтатусЭлектронногоСогласования().Code() > 405)
+        {
+            label10.Visible = true;
+            if (ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 406).Count() > 0)
+                label11.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 406).OrderByDescending(x => x.Date).First().Date.ToString();
+        }
+
+
+        //            System.Windows.Forms.MessageBox.Show((ШапкаЗПР.ИсторияСогласованияList().Count).ToString(), "колво");*/
+
+
+        /*bool a = true;
+        if (ШапкаЗПР.СтатусЭлектронногоСогласования().As<СостояниеЗапроса>() == СостояниеЗапроса.ВРаботеЦехов)
+        label15.Visible = a;  */
+    }
+}
+
+private void СогласованиеЗапроса_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+{
+
+    if (ШапкаЗПР.ГруппаЗаказов == null)
+    {
+        System.Windows.Forms.MessageBox.Show(String.Format("Это не ЗПР"), "Oops...", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation);
+        this.StopPageBuilding();
+        return;
+    }
+
+
+    if (ШапкаЗПР.СтатусЭлектронногоСогласования().Code() == 460)
+    {
+
+        if (ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 408).Count() > 0)
+        {
+            label40.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 408).OrderByDescending(x => x.Date).First().Сотрудник.Наименование.ToString();
+            label35.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 408).OrderByDescending(x => x.Date).First().Date.ToString();
+        }
+
+        if (ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 430).Count() > 0)
+        {
+            label39.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 430).OrderByDescending(x => x.Date).First().Сотрудник.Наименование.ToString();
+            label34.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусКонечный.Code() == 430).OrderByDescending(x => x.Date).First().Date.ToString();
+        }
+
+        if (ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусНачальный.Code() == 445).Count() > 0)
+        {
+            label41.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусНачальный.Code() == 445).OrderByDescending(x => x.Date).First().Сотрудник.Наименование.ToString();
+            label33.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусНачальный.Code() == 445).OrderByDescending(x => x.Date).First().Date.ToString();
+        }
+
+        if (ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусНачальный.Code() == 450).Count() > 0)
+        {
+            label43.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусНачальный.Code() == 450).OrderByDescending(x => x.Date).First().Сотрудник.Наименование.ToString();
+            label32.Text = ШапкаЗПР.ИсторияСогласованияList().Where(x => x.СтатусНачальный.Code() == 450).OrderByDescending(x => x.Date).First().Date.ToString();
+        }
+    }
+    else ReportFooter1.Visible = false;
+
+
+}
+
